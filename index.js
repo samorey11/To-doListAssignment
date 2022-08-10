bdde5a4bb970399389bf8e655fbd2f791474f59e
Sam Morey <samorey@outlook.com>
2022-08-09 23:43:25 -0400

    Getting list from DOM, trying to format list

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
-$.ajax({
-  type: 'GET',
-  url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=510' ,
-  dataType: 'json',
-  success: function (response, textStatus) {
-    console.log(response);
-    // response is a parsed JavaScript object instead of raw JSON
-  },
-  error: function (request, textStatus, errorMessage) {
-    console.log(errorMessage);
-  }
+$(document).ready(function () {
+  console.log('dom ready');
+
+  $.ajax({
+    type: 'GET',
+    url: "https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=512",
+    //contentType: 'application/json',
+    dataType: 'json',
+    //data: JSON.stringify({
+      //task: {
+        //content: 'Go to the gym'
+      //}
+    //}),
+    success: function (response, textStatus) {
+      var taskList = response.tasks.forEach(function (task) {
+        task = task.content;
+
+        
+        $('#todo-list').append('<p><input type="checkbox" value="completed">' + task + '<button type="button" class="btn btn-danger">Remove</button></p>');
+      });
+    },
+    error: function (request, textStatus, errorMessage) {
+      console.log(errorMessage);
+    }
+  });
 });
+
